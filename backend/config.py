4c6f2de8c46c0751d2ccc17a80a41b34ceddb112
yassinOrlando <yassinovp@gmail.com>
2021-06-25 18:39:27 -0500

    connection to the DB working and first route created. Next I will add more SQL queries

--- backend/config.py
+++ backend/config.py
@@ -1,7 +1,24 @@
+import pyodbc
 from flask import Flask, jsonify
 
 app = Flask(__name__)
 
+cnxn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER=DESKTOP-JCTSONC\MSSQLSERVER01;Database=renave_db;Trusted_Connection=yes;')
+#cnxn = pyodbc.connect('DRIVER=sqlServerDb;SERVER=DESKTOP-JCTSONC\MSSQLSERVER01;Trusted_Connection=yes;')
+
+
 @app.route("/")
 def hello_world():
-    return jsonify({'message': "<p>Hello, World!</p>"})
+    cursor = cnxn.cursor()
+    colores = cursor.execute("SELECT * FROM colores").fetchall()
+    coloresList = []
+    for color in colores:
+        coloresList.append(
+            {
+                'id': color[0],
+                'nombre': color[1],
+            }
+        )
+    print(colores)
+    return jsonify(coloresList)
+    #return colores
